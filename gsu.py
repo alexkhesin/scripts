a0926b5d64178e872c12e4c09e3b1feb40e168f8
Alex Khesin <alex@khesin.com>
2015-06-03 11:05:59 -0400

    pdftotext version

--- gsu.py
+++ gsu.py
@@ -1,82 +1,7 @@
 import datetime
 import decimal
 import sys
-import getopt
 import os.path
-
-header1 = """  Transaction Detail
-Security Name:
-Trading Symbol:
-Plan Name / Plan Number:
-Award Date:
-Award Type:
-Award ID:
-Award Price:
-Release Date:
-*FMV @ Vest / FMV Date:
-Quantity Released:
-Quantity Withheld:
-Net Quantity:
-Total Gain (FMV x Quantity Released):
-Aggregate Award Price:
-Taxable Compensation:
-  Total Release Cost Calculation
-Total Aggregate Award Price:
-Total Tax Amount:
-Total Release Cost:
-Primary Payment Method: WTC
-**Excess Amount:
-*Fair Market Value (FMV) is calculated according to the Company's Plan.
-**This Excess Amount will be paid to you via your Company payroll in accordance with your Company's instructions
-  Additional Information — Please retain this confirmation for your tax records
-""".split('\n')
-
-header2 = """Summary for Release
-Quantity Released:
-Total Release Cost Detailed Below:
-Quantity Withheld to Pay for Release:
-**Excess Amount:
-Net Quantity:
-
-Federal Tax
-Medicare Tax
-State Tax
-Local1 Tax
-
-Tax %
-39.6000 %
-2.3500 %
-9.6200 %
-4.2500 %
-
-© 2014 Morgan Stanley Smith Barney LLC. Member SIPC.
-""".split('\n')
-
-header2a = """Summary for Release
-Quantity Released:
-Total Release Cost Detailed Below:
-Quantity Withheld to Pay for Release:
-**Excess Amount:
-Net Quantity:
-""".split('\n')
-
-class SeekToHeader:
-  def __init__(self, hdr):
-    self._hdr = hdr
-  def next(self, line):
-    return line != self._hdr[0]
-
-class SeekPastHeader:
-  def __init__(self, hdr):
-    self._hdr = hdr
-    self._idx = 1
-
-  def next(self, line):
-    if line != self._hdr[self._idx]:
-      raise Exception("H" + str(self._idx) + ": [" + line + "] expected [" +
-                      self._hdr[self._idx] + "]")
-    self._idx += 1
-    return self._idx < len(self._hdr)
 
 def text(s): return s
 def number(s): return decimal.Decimal(s.replace(',', ''))
@@ -91,106 +16,80 @@ def paren_number(s):
   if s[0] != '(' or s[-1] != ')':
     raise Exception("Expected parentheses: [" + s + "]")
   return number(s[1:-1])
+def paren_currency(s):
+  if s[0] != '(' or s[-1] != ')':
+    raise Exception("Expected parentheses: [" + s + "]")
+  return currency(s[1:-1])
 
-fields1 = (('name', text), ('ticker', text), ('plan', text),
-           ('award_date', text), (None, 'Restricted Stock Units'),
-           ('award_id', text), (None, '$0.0000'), ('date', date),
-           ('price', price_date), ('shares', number),
-           ('shares_withheld', paren_number),
-           ('shares_net', number), ('gross', currency))
-fields2 = (('cusip', text), (None, ''), (None, ''), (None, ''), (None, ''),
-           ('shares', number), ('taxes', currency),
-           ('shares_withheld', number), ('excess', currency),
-           ('shares_net', number), (None, ''),
-           ('price', currency),
-           ('withheld_total', currency), (None, ''),
-           ('shares_withheld', number), (None, ''), (None, 'Tax Paid'),
-           ('tax_fed', currency), ('tax_med', currency),
-           ('tax_nys', currency), ('tax_nyc', currency))
-fields2a = (('shares', number), ('taxes', currency),
-            ('shares_withheld', number), ('excess', currency),
-            ('shares_net', number), (None, ''),
-            ('price', currency),
-            ('withheld_total', currency), (None, ''),
-            ('shares_withheld', number), (None, ''), (None, 'Tax Paid'),
-            ('tax_fed', currency), ('tax_med', currency),
-            ('tax_nys', currency), ('tax_nyc', currency))
-fields2b = (('price', currency),
-            ('withheld_total', currency), (None, ''),
-            ('shares_withheld', number), (None, ''), (None, 'Tax Paid'),
-            ('tax_fed', currency), ('tax_med', currency),
-            ('tax_nys', currency), ('tax_nyc', currency), (None, ''),
-            ('shares', number), ('taxes', currency),
-            ('shares_withheld', number), ('excess', currency),
-            ('shares_net', number))
-
-class Capture:
-  def __init__(self, rec, fields):
-    self._idx = 0
-    self._rec = rec
-    self._fields = fields
-
-  def next(self, line):
-    f = self._fields[self._idx]
-    if not f[0]:
-      if line != f[1]:
-        raise Exception("C" + str(self._idx) + ": [" + line + "] expected [" +
-                        f[1] + "]")
-    else:
-      new = f[1](line)
-      if f[0] in self._rec:
-        old = self._rec[f[0]]
-        if old != new:
-          raise Exception("old value [%s] does not match new [%s]" % (old, new))
-      else:
-        self._rec[f[0]] = new
-
-    self._idx += 1
-    return self._idx < len(self._fields)
-
-class ConsumeRest:
-  def next(self, line): return True
-
-alt = None
-post_split = True
-opts, args = getopt.getopt(sys.argv[1:],"abo")
-for opt, arg in opts:
-  if opt == '-a': alt = 'a'
-  if opt == '-b': alt = 'b'
-  if opt == '-o': post_split = False
+fields = (
+'Release Detail Report',
+'Total Release Cost Calculation',
+'Summary for Release',
+('Quantity Released: ', number, 'shares'),  # 30.0000
+('Total Release Cost Detailed Below: ', currency, 'taxes'), # $6,629.30
+('Quantity Withheld to Pay for Release: ', number, 'shares_withheld'), # 13.0000
+('**Excess Amount: ', currency, 'excess'), # $339.87
+('Net Quantity: ', number, 'shares_net'), # 17.000000
+'Transaction Detail',
+('Security Name: ', text, 'name'), # GOOGLE INC CL C
+('Trading Symbol: ', text, 'ticker'), # GOOG
+('Plan Name / Plan Number: ', text, 'plan'), # 2012 Stock Option Plan / C2012
+('Award Date: ', date, 'award_date'), # 02-Jul-2014
+'Award Type: Restricted Stock Units',
+('Award ID: ', text, 'award_id'), # C148136
+'Award Price: $0.0000',
+('Release Date: ', date, 'date'), # 25-Feb-2015
+('*FMV @ Vest / FMV Date: ', price_date, 'price'), # $536.0900 / 25-Feb-2015
+'Additional Information — Please retain this confirmation for your tax'
+' records',
+'Total Aggregate Award Price: $0.00',
+('Total Tax Amount: ', currency, 'taxes'), #  $6,629.30
+('Total Release Cost: ', paren_currency, 'taxes'),  #  ($6,629.30)
+('Primary Payment Method: WTC ', currency, 'withheld_total'), # $6,969.17
+('**Excess Amount: ', currency, 'excess'), # $339.87
+'*Fair Market Value (FMV) is calculated according to the Company\'s Plan.',
+'**This Excess Amount will be paid to you via your Company payroll in'
+' accordance with your Company\'s instructions',
+('Quantity Released: ', number, 'shares'), #  30.0000
+('Quantity Withheld: ', paren_number, 'shares_withheld'), #  (13.0000)
+('Net Quantity: ', number, 'shares_net'), # 17.0000
+('Total Gain (FMV x Quantity Released): ', currency, 'gross'), # $16,082.70
+'Aggregate Award Price: $0.00',
+('Taxable Compensation: ', currency, 'gross'), # $16,082.70
+('Withheld Quantity: ', number, 'shares_withheld'), # 13.0000
+'x Withheld Quantity Value Per',
+('Share: ', currency, 'price'), # $536.09
+('Withheld Quantity Value: ', currency, 'withheld_total'), # $6,969.17
+'Tax Information',
+'Tax % Tax Paid',
+('Federal Tax 25.0000 % ', currency, 'tax_fed'), # $4,020.68
+('Medicare Tax 2.3500 % ', currency, 'tax_med'), # $377.95
+('State Tax 9.6200 % ', currency, 'tax_nys'), # $1,547.16
+('Local1 Tax 4.2500 % ', currency, 'tax_nyc')) # $683.51
 
 rec = {}
-
-if not alt:
-  seq = (SeekToHeader(header1), SeekPastHeader(header1),
-         Capture(rec, fields1),
-         SeekToHeader(header2), SeekPastHeader(header2),
-         Capture(rec, fields2),
-         ConsumeRest())
-else:
-  seq = (SeekToHeader(header1), SeekPastHeader(header1),
-         Capture(rec, fields1),
-         SeekToHeader(header2a), SeekPastHeader(header2a),
-         Capture(rec, fields2a if alt == 'a' else fields2b),
-         ConsumeRest())
-
-idx = 0
-curr = None
-
 i = 0
 for line in sys.stdin:
-  if not curr:
-    curr = seq[idx]
-    idx += 1
-  try:
-    if not curr.next(line.rstrip()):
-      curr = None
-  except:
-    print("Line ", i)
-    raise
+  line = line.rstrip()
+  if i < len(fields):
+    f = fields[i]
+    if isinstance(f, str):
+      if line != f:
+        raise Exception("line %i expected [%s], found [%s]" % (i, f, line))
+    else:
+      prefix = f[0]
+      if not line.startswith(prefix):
+        raise Exception("line %i should start with [%s]: [%s]" %
+                        (i, prefix, line))
+      new = f[1](line[len(prefix):])
+      if f[2] in rec:
+        old = rec[f[2]]
+        if old != new:
+          raise Exception("line %i old value [%s] does not match new [%s]: [%s]"
+                          % (i, old, new, line))
+      else:
+        rec[f[2]] = new
   i += 1
-if idx != len(seq):
-  raise Exception("short")
 
 # consistency checks
 if (rec['tax_fed'] + rec['tax_med'] + rec['tax_nys'] + rec['tax_nyc'] !=
@@ -202,16 +101,11 @@ if rec['taxes'] + rec['excess'] != rec['withheld_total']:
   raise Exception('witholdings do not add up')
 if rec['price'] * rec['shares_withheld'] != rec['withheld_total']:
   raise Exception('bad witholding total')
-if post_split:
-  if rec['ticker'] == 'GOOG':
-    if rec['name'] != 'GOOGLE INC CL C':
-      raise Exception('bad class C')
-  elif (rec['ticker'] != 'GOOGL' or rec['name'] != 'GOOGLE INC-CL A'):
-    raise Exception('bad class A')
-else:
-  if rec['ticker'] == 'GOOG':
-    if rec['name'] != 'GOOGLE INC-CL A':
-      raise Exception('bad presplit class A')
+if rec['ticker'] == 'GOOG':
+  if rec['name'] != 'GOOGLE INC CL C':
+    raise Exception('bad class C')
+elif (rec['ticker'] != 'GOOGL' or rec['name'] != 'GOOGLE INC-CL A'):
+  raise Exception('bad class A')
 if rec['shares'] * rec['price'] != rec['gross']:
   raise Exception('shares*price != gross')
 
